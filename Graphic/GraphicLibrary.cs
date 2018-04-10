fbf7bbc39592f20f89e9698b041ec022f3056b3b
RedBomb1811 <dimadudko00@gmail.com>
2018-04-10 15:03:34 +0300

    new rep

---
+++ Graphic/GraphicLibrary.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphic
+{
+    class Screen
+    {
+        readonly int resolution_X;
+        readonly int resolution_Y;
+        byte[,] screenArray;
+        readonly int[] density = new int[7] {
+                0x2588,
+                0x2593,
+                0x2592,
+                0x2591,
+                0x2219, // центральная точка
+                0x00B7,  // центральный кубик
+                0x00A0
+            };
+
+        // 0x0387 маленькая центральная точка
+
+        public int Resolution_X { get => resolution_X; }
+        public int Resolution_Y { get => resolution_Y; }
+        public byte[,] ScreenArray { get => screenArray; set => screenArray = value; }
+        public int[] Density => density;
+
+        public byte this[int i, int j]
+        {
+            get => ScreenArray[i, j];
+            set => ScreenArray[i, j] = value;
+        }
+
+        public Screen(int x, int y)
+        {
+            resolution_X = x;
+            resolution_Y = y;
+            ScreenArray = new byte[Resolution_Y, Resolution_X];
+            for (int i = 0; i < Resolution_Y; i++)
+                for (int j = 0; j < Resolution_X; j++)
+                    ScreenArray[i, j] = 0;
+
+            Console.SetWindowSize(Resolution_X, Resolution_Y);
+            Console.SetBufferSize(Resolution_X, Resolution_Y);
+            Console.Title = "Screen";
+        }
+
+        public void RedrawScreen_2()
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < Resolution_Y; i++)
+            {
+                for (int j = 0; j < Resolution_X; j++)
+                {
+                    switch (this[i, j])
+                    {
+                        case 0:
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            break;
+                        case 1:
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            break;
+                        case 2:
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            break;
+                        case 3:
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            break;
+                        case 4:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+                        case 5:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            break;
+                        case 6:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                    }
+                    Console.Write((char)0x2219);
+                }
+            }
+        }
+
+        public void ChangeColor(ConsoleColor color ) {
+            Console.ForegroundColor = color;
+        }
+
+        public void RedrawScreen()
+        {
+            Console.SetCursorPosition(0, 0);
+            string str = "";
+            for (int i = 0; i < Resolution_Y; i++)
+            {
+                for (int j = 0; j < Resolution_X; j++)
+                {
+                    str += (char)Density[ScreenArray[i, j]];
+        //            str += (char)density[(int)(i * Density.Length / Resolution_Y)];
+                }
+            }
+            Console.Write(str);
+        }
+
+        public void FillScreen(byte a)
+        {
+            for (int i = 0; i < Resolution_Y; i++)
+            {
+                for (int j = 0; j < Resolution_X; j++)
+                {
+                    ScreenArray[i, j] = a;
+                }
+            }
+        }
+
+        public void DrawFromEquation(Func<int, int, int> function, int c, byte a)
+        {
+        //    for (int y = 0; y < Resolution_Y; y++)
+            for (int x = 0; x < Resolution_X; x++)
+            {
+                if(function(x, c) >= 0 && function(x, c) < Resolution_Y)
+                    ScreenArray[function(x, c), x] = a;
+            }
+        }
+
+        public void WriteSymbol(char sym, int x, int y)
+        {
+            byte[,] arrsym;
+            switch (sym)
+            {
+                case '0':
+                    arrsym = new byte[,] { 
+                        { 0, 0, 1, 1, 0, 0 }, 
+                        { 0, 1, 0, 0, 1, 0 }, 
+                        { 1, 0, 0, 0, 0, 1 }, 
+                        { 1, 0, 0, 0, 0, 1 }, 
+                        { 1, 0, 0, 0, 0, 1 }, 
+                        { 1, 0, 0, 0, 0, 1 }, 
+                        { 0, 1, 0, 0, 1, 0 }, 
+                        { 0, 0, 1, 1, 0, 0 }
+                    };
+                    break;
+                case '1':
+                    arrsym = new byte[,] {
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 1, 1, 1, 0, 0 },
+                        { 1, 1, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 1, 1, 1, 1, 1, 1 }
+                    };
+                    break;
+                case '2':
+                    arrsym = new byte[,] {
+                        { 0,  0,  1,  1,  0,  0 },
+                        { 0,  1,  0,  0,  1,  0 },
+                        { 1,  0,  0,  0,  0,  1 },
+                        { 0,  0,  0,  0,  0,  1 },
+                        { 0,  0,  0,  0,  1,  0 },
+                        { 0,  0,  1,  1,  0,  0 },
+                        { 0,  1,  0,  0,  0,  0 },
+                        { 1,  1,  1,  1,  1,  1 }
+                    };
+                    break;
+                case '3':
+                    arrsym = new byte[,] {
+                        { 1,  1,  1,  1,  1,  1 },
+                        { 0,  0,  0,  0,  1,  0 },
+                        { 0,  0,  0,  1,  0,  0 },
+                        { 0,  0,  0,  0,  1,  0 },
+                        { 0,  0,  0,  0,  0,  1 },
+                        { 1,  0,  0,  0,  0,  1 },
+                        { 0,  1,  0,  0,  1,  0 },
+                        { 0,  0,  1,  1,  0,  0 }
+                    };
+                    break;
+                case '4':
+                    arrsym = new byte[,] {
+                        { 0,  0,  0,  0,  1,  0 },
+                        { 0,  0,  0,  1,  1,  0 },
+                        { 0,  0,  1,  0,  1,  0 },
+                        { 0,  1,  0,  0,  1,  0 },
+                        { 1,  0,  0,  0,  1,  0 },
+                        { 1,  1,  1,  1,  1,  1 },
+                        { 0,  0,  0,  0,  1,  0 },
+                        { 0,  0,  0,  0,  1,  0 }
+                    };
+                    break;
+                case '5':
+                    arrsym = new byte[,] {
+                        { 1,  1,  1,  1,  1,  1 },
+                        { 1,  0,  0,  0,  0,  0 },
+                        { 1,  1,  1,  1,  0,  0 },
+                        { 0,  0,  0,  0,  1,  0 },
+                        { 0,  0,  0,  0,  0,  1 },
+                        { 1,  0,  0,  0,  0,  1 },
+                        { 0,  1,  0,  0,  1,  0 },
+                        { 0,  0,  1,  1,  0,  0 }
+                    };
+                    break;
+                case '6':
+                    arrsym = new byte[,] {
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 1, 1, 1, 0, 0 },
+                        { 1, 1, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 1, 1, 1, 1, 1, 1 }
+                    };
+                    break;
+                case '7':
+                    arrsym = new byte[,] {
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 1, 1, 1, 0, 0 },
+                        { 1, 1, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 1, 1, 1, 1, 1, 1 }
+                    };
+                    break;
+                case '8':
+                    arrsym = new byte[,] {
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 1, 1, 1, 0, 0 },
+                        { 1, 1, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 1, 1, 1, 1, 1, 1 }
+                    };
+                    break;
+                case '9':
+                    arrsym = new byte[,] {
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 1, 1, 1, 0, 0 },
+                        { 1, 1, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 1, 0, 0 },
+                        { 1, 1, 1, 1, 1, 1 }
+                    };
+                    break;
+            }
+        }
+
+        public void DrawLine(int x1, int y1, int x2, int y2, byte color)
+        {
+            int X1 = Math.Min(x1, x2);
+            int X2 = Math.Max(x2, x1);
+            for (int x = 0; x < Resolution_X; x++) {
+                int Y = (x * (y2 - y1) + (X2 * y1 - X1 * y2)) / (X2 - X1);
+                if(Y >= 0 && Y < Resolution_Y)
+                    ScreenArray[Y, x] = color;
+            }
+        }
+
+        public void DrawLineSegment(int x1, int y1, int x2, int y2, byte color)
+        {
+            int X1 = Math.Min(x1, x2);
+            int X2 = Math.Max(x2, x1);
+            for (int x = X1; x <= X2; x++)
+            {
+                int Y = (x * (y2 - y1) + (X2 * y1 - X1 * y2)) / (X2 - X1);
+                if (Y >= 0 && Y < Resolution_Y && x >= 0 && x < Resolution_X)
+                    ScreenArray[Y, x] = color;
+            }
+        }
+
+        public void DrawPoint(int x, int y, byte color)
+        {
+            if (x >= 0 && x < Resolution_X && y >= 0 && y < Resolution_Y)
+                ScreenArray[y, x] = color;
+        }
+    }
+}
---
+++ Graphic/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphic
+{
+    class Program
+    {
+        // Font-Size 5 Max: 454x141 Coordinates(-4, 0)
+        // FullScreen font-size 5 Max: 453x139
+        // Font-Size 6 Max: 340x119 Coordinates(-4, 0)
+        // FullScreen font-size 6 Max: 339x115
+        // Font-Size 8 Max: 272x87 Coordinates(-4, 0)
+        // FullScreen font-size 8 Max: 
+        public static int Method(int variable)
+        {
+            return variable * variable;
+        }
+
+        static void Main(string[] args)
+        {
+            Func<int, int> F  = Method;
+            Screen screen = new Screen(272, 87);
+            screen.ChangeColor(ConsoleColor.Green);
+            screen.FillScreen(6);
+            screen.DrawLine(40, 80, 100, 10, 5);
+            screen.DrawLineSegment(40, 10, 10, 40, 3);
+            screen.DrawLineSegment(40, 30, 60, 10, 0);
+            screen.DrawPoint(40, 80, 0);
+            screen.DrawPoint(100, 10, 0);
+            int r = 0;
+            while (r < 50)
+            {
+                screen.DrawLine(100 + r, 1, 101 + r, 0, 3);
+                r += 2;
+            }
+            r = 0;
+            while (r < 30)
+            {
+                if (!(r > 10 && r < 19))
+                {
+                    screen.DrawLine(51 + r, 0, 52 + r, 1, 0);
+                    screen.DrawLine(50 + r, 0, 51 + r, 1, 0);
+                }
+                r += 2;
+            }
+            r = 0;
+            while (r < 10)
+            {
+                screen.DrawLineSegment(56, 75 + r, 150, 40 + r, 6);
+                r++;
+            }
+
+
+            screen.RedrawScreen();
+            Console.ReadKey();
+        }
+    }
+}
